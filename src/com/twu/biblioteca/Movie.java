02a75cfd7aabde775dc18c06d3a415221500c8fa
Aaron North <aarontnorth@minint-1sr2fc9.corporate.thoughtworks.com>
2020-02-07 15:34:16 -0600

    Added Movie class

---
+++ src/com/twu/biblioteca/Movie.java
@@ -0,0 +1,15 @@
+package com.twu.biblioteca;
+
+public class Movie {
+    String title;
+    String year;
+    String director;
+    int rating;
+
+    Movie(String title,String year,String director,int rating){
+        this.title = title;
+        this.year = year;
+        this.director = director;
+        this.rating = rating;
+    }
+}
